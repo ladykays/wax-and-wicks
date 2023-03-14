e986303627658a593043b1c2069edd394ab13a1c
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-14 22:58:51 +0000

    installed tailwind css and applied styles to navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,12 +5,18 @@ import "./navbar.css";
 
 export const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="logo">Wax & Wicks</div>
+    <div className="navbar flex justify-between items-center p-6 text-amber-900 bg-orange-50">
+      <div className="logo">
+        <h1 class="font-normal">
+          <a href="/" className="text-3xl">Wax & Wicks</a>
+        </h1>
+      </div>
       <div className="links">
-        <Link to="/"> Home </Link>
-        <Link to="/shop"> Shop </Link>
-        <Link to="/contact"> Contact </Link>
+        <Link to="/" className="link mr-8"> Home </Link>
+        <Link to="/shop" className="link mr-8"> Shop </Link>
+        <Link to="/contact" className="link"> Contact </Link>
+      </div>
+      <div>
         <Link to="/basket">
           <ShoppingCart size={32} />
         </Link>
