4b3d28881cd90361d09ee8a6922c3326fe97bccd
Ffion Erin <ffionb@btinternet.com>
2023-03-14 20:52:10 +0000

    changed app routes and commented out navbar and footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Pages/home/home'
 import Shop from './Pages/shop/shop'
 import Basket from "./Pages/basket/basket";
 import Contact from "./Pages/contact/contact";
@@ -8,11 +9,12 @@ import Contact from "./Pages/contact/contact";
 function App() {
   return (
     <div className="App">
-      <Router> 
+      <Router>
         <Routes>
-          <Route path="/" element={<Shop />} /> 
-          <Route path="/cart" element={<Basket />}/>
-          <Route path="/cart" element={<Contact />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Basket />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
