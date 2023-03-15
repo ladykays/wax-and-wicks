157955900e64bad369db12441bbe7a74a2ccf09a
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-15 22:15:10 +0000

    added contact form

---
+++ src/Pages/contact/contact.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import "./contact.css";
+
+export const Contact = () => {
+  // State variables
+  const [success, setSuccess] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+    access_key: "83dfac15-0283-4f4a-b449-b232d0b873fd",
+  });
+
+  //Callback functions
+  const handleChange = (event) => {
+    //use a setter to pass in a new object
+    setFormData({
+      //spread all the previous formdata properties and update/overwrite the current property that the user is typing into
+      ...formData,
+      [event.target.name]: event.target.value
+    });
+    //console.log(event.target.value);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // const formData = new FormData(form);
+    // const object = Object.fromEntries(formData);
+    const data = JSON.stringify(formData);
+
+      fetch('https://api.web3forms.com/submit', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Accept': 'application/json'
+              },
+              body: data
+          })
+            //When the message succeeds in sending, then this is called
+            .then(res => res.json()) 
+            .then(data => {
+              setSuccess(true);
+
+              setTimeout(() => {
+                setSuccess(false);
+              }, 3000);
+            })
+            .catch(err => console.log(err));
+  }; 
+
+  return (
+    <>
+      <h1 className="title">Contact</h1>
+
+      <form onSubmit={handleFormSubmit}>
+        <input name="name" onChange={handleChange} type="text" placeholder="Enter your Name" />
+        <input name="email" onChange={handleChange} type="text" placeholder="Enter Email Address" />
+        <textarea name="message" onChange={handleChange} placeholder="Enter your Message" cols="30" rows="10"></textarea>
+        <button>Submit</button>
+      </form>
+
+      {success && <p>Form submitted successfully!</p>}
+      
+    </>
+  )
+}
