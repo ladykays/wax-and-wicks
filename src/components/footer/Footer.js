fc3f45e4d292616db6d94c3b16d786291050dcdb
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-15 23:19:50 +0000

    fixed link not opening on new browser window

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,20 +7,39 @@ export const Footer = () => {
       <div className="flex justify-center">
         <ul className="list-group">
           <li className="list-item ">
-           <a href="https://github.com/ladykays" >
-            <i class="fa-brands fa-github mr-1"></i>Ladykays</a> 
+            <a 
+              href="https://github.com/ladykays"
+              target="_blank"
+              rel="noreferrer" >
+                <i class="fa-brands fa-github mr-1"></i>
+                Ladykays
+            </a> 
           </li>
           <li className="list-item ">
-           <a href="https://github.com/Jessie93" target="_blank" rel="noreferrer">
-            <i class="fa-brands fa-github mr-1"></i>Jessie93</a> 
+           <a 
+            href="https://github.com/Jessie93" 
+            target="_blank" 
+            rel="noreferrer">
+              <i class="fa-brands fa-github mr-1"></i>
+              Jessie93
+            </a> 
           </li>
           <li className="list-item ">
-           <a href="https://github.com/ffionerin" target="_blank" rel="noreferrer">
-            <i class="fa-brands fa-github mr-1"></i>Ffionerin</a> 
+            <a href="https://github.com/ffionerin" 
+              target="_blank" 
+              rel="noreferrer">
+                <i class="fa-brands fa-github mr-1"></i>
+                Ffionerin
+            </a> 
           </li>
           <li className="list-item mr-0">
-           <a href="https://github.com/Temioye" target="_blank" rel="noreferrer">
-            <i class="fa-brands fa-github mr-1"></i>Temioye</a> 
+            <a 
+              href="https://github.com/Temioye" 
+              target="_blank" 
+              rel="noreferrer">
+                <i class="fa-brands fa-github mr-1"></i>
+                Temioye
+            </a> 
           </li>
         </ul>
       </div>
