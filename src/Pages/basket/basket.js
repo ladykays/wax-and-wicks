ab53e3206a44d1148b479bc66815e15b03b17475
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-15 18:27:35 +0000

    moved some css to css file

--- src/Pages/basket/basket.js
+++ src/Pages/basket/basket.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { products } from "../../products";
 import "./basket.css";
 
 export const Basket = () => {
+
   return (
     <div className="basket">
       <h1 className="mt-4 font-size: 20px">Your Shopping Basket</h1>
+      <div className="basket-items">
+        
+      </div>
 
-      <div className="checkout flex sm:flex-row flex-col justify-center items-center">
+      <div className="buttons-container">
         <button className="btn mr-4">Continue Shopping</button>
         <button className="btn">Checkout</button>
       </div>
---
+++ src/products.js
@@ -0,0 +1,47 @@
+//Faking a jason for our products.
+import product1 from "./assets/images/image2.png";
+
+const products = [
+  {
+    id: 1,
+    name: "XXX candle",
+    imageSrc: product1,
+    imageAlt: "Photo of a XX",
+    price: "£7.99",
+  },
+  {
+    id: 2,
+    name: "XXX candle",
+    imageSrc: product1,
+    imageAlt: "Photo of a XX",
+    price: "£7.99",
+  },
+  {
+    id: 3,
+    name: "XXX candle",
+    imageSrc: product1,
+    imageAlt: "Photo of a XX",
+    price: "£7.99",
+  },
+  {
+    id: 4,
+    name: "XXX candle",
+    imageSrc: product1,
+    imageAlt: "Photo of a XX",
+    price: "£7.99",
+  },
+  {
+    id: 5,
+    name: "XXX candle",
+    imageSrc: product1,
+    imageAlt: "Photo of a XX",
+    price: "£7.99",
+  },
+  {
+    id: 6,
+    name: "XXX candle",
+    imageSrc: product1,
+    imageAlt: "Photo of a XX",
+    price: "£7.99",
+  },
+];
