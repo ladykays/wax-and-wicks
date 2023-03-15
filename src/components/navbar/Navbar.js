7b09c3707acdd8d2cbd76624b61019b7a09bf113
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-15 13:58:32 +0000

    moved styles to navbar.css to make code readable

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,19 +5,19 @@ import "./navbar.css";
 
 export const Navbar = () => {
   return (
-    <div className="navbar flex justify-between items-center p-6 text-amber-900 bg-orange-50">
+    <div className="navbar">
       <div className="logo">
         <h1 class="font-normal">
           <a href="/" className="text-3xl">Wax & Wicks</a>
         </h1>
       </div>
       <div className="links">
-        <Link to="/" className="link mr-8 hover:text-amber-600"> Home </Link>
-        <Link to="/shop" className="link mr-8 hover:text-amber-600"> Shop </Link>
-        <Link to="/contact" className="link hover:text-amber-600"> Contact </Link>
+        <Link to="/" className="link"> Home </Link>
+        <Link to="/shop" className="link"> Shop </Link>
+        <Link to="/contact" className="link"> Contact </Link>
       </div>
       <div className="links">
-        <Link to="/basket" className="link hover:text-amber-600">
+        <Link to="/basket" className="link">
           <ShoppingCart size={32} />
         </Link>
       </div>
