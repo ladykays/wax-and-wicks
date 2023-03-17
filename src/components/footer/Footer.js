cfb9ef763901f53924ad8fdff92868e5e9b6fb0a
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-17 19:06:36 +0000

    fixed links so they could open on a new window so as not to take user away from the site

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./footer.css"
+import "./footer.css";
 
 export const Footer = () => {
   return (
@@ -7,26 +7,50 @@ export const Footer = () => {
       <div className="flex justify-center">
         <ul className="flex items-center sm:flex-row flex-col text-md text-amber-900">
           <li className="list-item ">
-           <a href="https://github.com/ladykays">
-            <i class="fa-brands fa-github mr-1"></i>Ladykays</a> 
+            <a
+              href="https://github.com/ladykays"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="fa-brands fa-github mr-1"></i>
+              Ladykays
+            </a>
           </li>
           <li className="list-item ">
-           <a href="https://github.com/Jessie93">
-            <i class="fa-brands fa-github mr-1"></i>Jessie93</a> 
+            <a
+              href="https://github.com/Jessie93"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="fa-brands fa-github mr-1"></i>
+              Jessie93
+            </a>
           </li>
           <li className="list-item ">
-           <a href="https://github.com/ffionerin">
-            <i class="fa-brands fa-github mr-1"></i>Ffionerin</a> 
+            <a
+              href="https://github.com/ffionerin"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="fa-brands fa-github mr-1"></i>
+              Ffionerin
+            </a>
           </li>
           <li className="list-item mr-0">
-           <a href="https://github.com/Temioye">
-            <i class="fa-brands fa-github mr-1"></i>Temioye</a> 
+            <a
+              href="https://github.com/Temioye"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i class="fa-brands fa-github mr-1"></i>
+              Temioye
+            </a>
           </li>
         </ul>
       </div>
       <div className="copyright p-10 text-gray-400">
-          <p>© Copyright 2023</p>
+        <p>© Copyright 2023</p>
       </div>
     </div>
-  )
-}
+  );
+};
