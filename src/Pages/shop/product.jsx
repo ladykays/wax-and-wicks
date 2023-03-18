249343b721edfc07231f037b7bfcfab01c5dd144
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-18 20:51:32 +0000

    moved some styles from product.jsx and shop.js to shop.css

--- src/Pages/shop/product.jsx
+++ src/Pages/shop/product.jsx
@@ -1,6 +1,7 @@
 //useContext is a hook that comes with React
 import React, { useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
+import "./shop.css";
 
 export const Product = (props) => {
   const { id, name, price, imageSrc, imageAlt } = props.data;
@@ -10,17 +11,17 @@ export const Product = (props) => {
   return (
     <div className="product group relative">
       <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-        <img className="h-full w-full object-cover object-center lg:h-full lg:w-full" src={imageSrc} alt={imageAlt} />
+        <img className="image" src={imageSrc} alt={imageAlt} />
       </div>
       <div className="mt-4 flex justify-between">
       <div>
-      <h3 className="text-sm text-gray-700">
+      <h3 className="name">
           {name}
         </h3>
       </div>
-      <p className="text-sm font-medium text-gray-900">£{price}</p>
+      <p className="price">£{price}</p>
       </div>
-      <button className="addToBasketBtn ml-2 px-3 py-1 rounded-md text-white" onClick={() => addToBasket(id)}>
+      <button className="addToBasketBtn" onClick={() => addToBasket(id)}>
         {/* Display basketItemAmount only if basketItemAmount is greater than 0 */}
         Add to Basket {basketItemAmount > 0 && <> ({basketItemAmount})</>}
       </button>
--- src/Pages/shop/shop.js
+++ src/Pages/shop/shop.js
@@ -8,7 +8,7 @@ export const Shop = () => {
   return (
     <div>
       <div
-        className="relative overflow-hidden rounded-lg bg-cover bg-no-repeat p-12 text-center"
+        className="hero"
         style={{
           backgroundImage:
             "url('https://images.unsplash.com/photo-1476900164809-ff19b8ae5968?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80')",
@@ -19,12 +19,12 @@ export const Shop = () => {
           className="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-fixed"
           style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}
         >
-          <div className="flex h-full items-center justify-center">
+          <div className="hero-text-container">
             <div className="text-white">
-              <h2 className="mb-4 text-4xl font-semibold">
+              <h2 className="hero-text">
                 Indulge in the warmth and comfort of Wax and Wicks.
               </h2>
-              <h4 className="mb-6 text-xl font-semibold">
+              <h4 className="hand-made">
                 Here you will find a range of hand-poured candles made with care and love
               </h4>
             </div>
@@ -32,12 +32,12 @@ export const Shop = () => {
         </div>
       </div>
       <div className="bg-white">
-        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="products-container">
           <div className="products">
-            <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            <h2 className="title">
               Check out our current candle range below
             </h2>
-            <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            <div className="stock">
               {/* Map through every element in the products array and grab the data from each of them */}
               {products.map((product) => (
                 <Product data={product} />
