4dddfd3b7339f04c98af6b327b4aebfc6a412ed7
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-18 03:14:20 +0000

    updated the checkout page to have the same color scheme as the site

--- src/Pages/checkout/checkout.js
+++ src/Pages/checkout/checkout.js
@@ -30,7 +30,22 @@ function getStepContent(step) {
   }
 }
 
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    primary: {
+      //light: '#757ce8',
+      main: '#783510',
+      dark: '#fb923d',
+      contrastText: '#fff',
+    },
+    /* secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText: '#000',
+    }, */
+  },
+});
 
 export default function Checkout() {
   const [activeStep, setActiveStep] = React.useState(0);
