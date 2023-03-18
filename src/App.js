cc7ca5738676fd7ceac122d04b024fe1be998b2c
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-18 17:16:22 +0000

    having problems with checkout.js so changed name to Checkout.jsx, added template files for checkout process, routed to checkout page, applied theme color to checkout and commented out irrelivant code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar/Navbar";
@@ -7,8 +6,8 @@ import Home from "./Pages/home/home"
 import { Shop } from './Pages/shop/shop'
 import { Basket } from "./Pages/basket/basket";
 import { Contact } from "./Pages/contact/contact";
+import Checkout from "./Pages/checkout/Checkout";
 import { ShopContextProvider } from "./context/shop-context";
-
 
 function App() {
   return (
@@ -20,6 +19,7 @@ function App() {
             <Route path="/" element={<Home />} /> 
             <Route path="/shop" element={<Shop />} /> 
             <Route path="/basket" element={<Basket />}/>
+            <Route path="/checkout" element={<Checkout />}/>
             <Route path="/contact" element={<Contact />}/>
           </Routes>
           <Footer />
