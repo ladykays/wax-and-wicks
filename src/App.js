93f12a5be9c7982390880d55feca43b0bada4b61
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-18 17:33:56 +0000

    .

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar/Navbar";
 import { Footer } from "./components/footer/Footer";
-import Home from "./Pages/home/home"
-import { Shop } from './Pages/shop/shop'
+import Home from "./Pages/home/home";
+import { Shop } from './Pages/shop/shop';
 import { Basket } from "./Pages/basket/basket";
 import { Contact } from "./Pages/contact/contact";
 import Checkout from "./Pages/checkout/Checkout";
