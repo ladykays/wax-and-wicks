4ccae3701fde64496906aadcefa5bf8267a330a3
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-18 02:37:30 +0000

    added files for the checkout process from mui templates and routed to the checkout page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar/Navbar";
@@ -7,6 +6,7 @@ import Home from "./Pages/home/home"
 import { Shop } from './Pages/shop/shop'
 import { Basket } from "./Pages/basket/basket";
 import { Contact } from "./Pages/contact/contact";
+import Checkout from "./Pages/checkout/Checkout";
 import { ShopContextProvider } from "./context/shop-context";
 
 
@@ -20,6 +20,7 @@ function App() {
             <Route path="/" element={<Home />} /> 
             <Route path="/shop" element={<Shop />} /> 
             <Route path="/basket" element={<Basket />}/>
+            <Route path="/checkout" element={<Checkout />}/>
             <Route path="/contact" element={<Contact />}/>
           </Routes>
           <Footer />
