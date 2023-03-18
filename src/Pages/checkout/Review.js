29ac73716f1c7f3766e46fba3e78f537be2bb874
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-18 17:32:03 +0000

    linked Review.js to products.js to pull products, changed currency and added a function to generate order number as soon as a customer checksout

--- src/Pages/checkout/Review.js
+++ src/Pages/checkout/Review.js
@@ -4,30 +4,31 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Grid from '@mui/material/Grid';
+import { products } from "../../products";
 
-const products = [
+/* const products = [
   {
     name: 'Product 1',
     desc: 'A nice thing',
-    price: '$9.99',
+    price: '£9.99',
   },
   {
     name: 'Product 2',
     desc: 'Another thing',
-    price: '$3.45',
+    price: '£3.45',
   },
   {
     name: 'Product 3',
     desc: 'Something else',
-    price: '$6.51',
+    price: '£6.51',
   },
   {
     name: 'Product 4',
     desc: 'Best thing of all',
-    price: '$14.11',
+    price: '£14.11',
   },
   { name: 'Shipping', desc: '', price: 'Free' },
-];
+]; */
 
 const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
 const payments = [
@@ -54,7 +55,7 @@ export default function Review() {
         <ListItem sx={{ py: 1, px: 0 }}>
           <ListItemText primary="Total" />
           <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
-            $34.06
+            £34.06
           </Typography>
         </ListItem>
       </List>
