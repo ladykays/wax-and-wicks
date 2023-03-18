d7e4ca4c9724466d1645e1f457cfc6fbe254ca63
Kerrion Siokwu <ladykerrion@sky.com>
2023-03-18 19:26:22 +0000

    added code to display the quantity of each item in the basket to the user at checkout before placing order

--- src/Pages/checkout/Review.js
+++ src/Pages/checkout/Review.js
@@ -1,50 +1,29 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Grid from '@mui/material/Grid'; 
-import { products } from '../../products';
-//import { Basket } from "../basket/basket"; 
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Grid from "@mui/material/Grid";
+import { products } from "../../products";
+//import { Basket } from "../basket/basket";
 import { ShopContext } from "../../context/shop-context";
 
-/* const products = [
-  {
-    name: 'Product 1',
-    desc: 'A nice thing',
-    price: '£9.99',
-  },
-  {
-    name: 'Product 2',
-    desc: 'Another thing',
-    price: '£3.45',
-  },
-  {
-    name: 'Product 3',
-    desc: 'Something else',
-    price: '£6.51',
-  },
-  {
-    name: 'Product 4',
-    desc: 'Best thing of all',
-    price: '£14.11',
-  },
-  { name: 'Shipping', desc: '', price: 'Free' },
-]; */
 
-const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
+const addresses = ["1 MUI Drive", "Reactville", "Anytown", "99999", "USA"];
 const payments = [
-  { name: 'Card type', detail: 'Visa' },
-  { name: 'Card holder', detail: 'Mr John Smith' },
-  { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
-  { name: 'Expiry date', detail: '04/2024' },
+  { name: "Card type", detail: "Visa" },
+  { name: "Card holder", detail: "Mr John Smith" },
+  { name: "Card number", detail: "xxxx-xxxx-xxxx-1234" },
+  { name: "Expiry date", detail: "04/2024" },
 ];
 
 export default function Review() {
   const { basketItems, getTotalBasketAmt } = React.useContext(ShopContext);
-  const totalAmt = getTotalBasketAmt();
+  const totalAmt = getTotalBasketAmt().toFixed(2);
 
-  const basketContent = products.filter(product => basketItems[product.id] !== 0);
+  const basketContent = products
+    .filter((product) => basketItems[product.id] !== 0)
+    .map((product) => ({ ...product, quantity: basketItems[product.id] }));
 
   return (
     <React.Fragment>
@@ -54,10 +33,8 @@ export default function Review() {
       <List disablePadding>
         {basketContent.map((product) => (
           <ListItem key={product.id}>
-            <ListItemText primary={product.name} />
-            <Typography variant="subtitle1">
-              £{product.price}
-            </Typography>
+            <ListItemText primary={`${product.name} (${product.quantity})`} />
+            <Typography variant="subtitle1">£{product.price}</Typography>
           </ListItem>
         ))}
         <ListItem sx={{ py: 1, px: 0 }}>
@@ -73,7 +50,7 @@ export default function Review() {
             Shipping
           </Typography>
           <Typography gutterBottom>John Smith</Typography>
-          <Typography gutterBottom>{addresses.join(', ')}</Typography>
+          <Typography gutterBottom>{addresses.join(", ")}</Typography>
         </Grid>
         <Grid item container direction="column" xs={12} sm={6}>
           <Typography variant="h6" gutterBottom sx={{ mt: 2 }}>
