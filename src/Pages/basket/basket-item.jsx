7d0b57154650e4952bf29b9f5488014fd9e4fc24
temio <temioyelade8@gmail.com>
2023-03-22 15:59:38 +0000

    updated dimensions

--- src/Pages/basket/basket-item.jsx
+++ src/Pages/basket/basket-item.jsx
@@ -10,7 +10,7 @@ export const BasketItem = (props) => {
   return (
     <div className="basketItem grid gap-1 flex justify-center">
       <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
-      <img className="image h-32 w-32 object-cover object-center lg:h-full lg:w-full" src={imageSrc} alt={imageAlt} />
+      <img className="image h-40 w-40 object-cover object-center lg:h-full lg:w-full" src={imageSrc} alt={imageAlt} />
       </div>
       <div className="t-4 flex justify-between">
         <div>
